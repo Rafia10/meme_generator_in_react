330080c09e55d85c6d397820b8d3c1a736242963
Rafia-Qadir <you@example.com>
2023-02-23 14:42:31 +0500

    meme generator is updated

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,11 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <img src="src/assets/react.svg" className="header--image" />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+        alt="React Logo"
+        className="header--image"
+      />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
